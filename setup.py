32034b0b07facdacdebf87ea58640467f79f9266
Rose Griffin <rosegriffin752@gmail.com>
2026-04-08 18:17:11 -0400

    Fix setup

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name="american_dialect_identification", packages=find_packages())
+setup(name="american_dialect_identification",
+      version="0.1",
+      packages=find_packages(where="src"),
+      package_dir={"": "src"})
